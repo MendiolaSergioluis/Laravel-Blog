734b7454202e28a1881f9e5de606a29d91d074f1
Sergioluis Mendiola <mendiolasergioluis@gmail.com>
2023-09-02 12:51:47 -0400

    make:model Post -mfc y configuracion

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id'       => User::all()->random(),
+            'category_id'   => Category::all()->random(),
+            'title'         => $title = fake()->unique()->sentence(),
+            'slug'          => Str::slug($title),
+            'excerpt'       => fake()->paragraph(30),
+            'body'          => fake()->paragraph(80)
+        ];
+    }
+}
